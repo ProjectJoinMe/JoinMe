dbbe3ac1ed2f222f08160b8215fb4b75dcab17a9
Cleatuer <nicole.wresnik@gmail.com>
2016-10-13 00:02:32 +0200

    [setup] Create maven project + configure Spring Boot and add ExampleController

---
+++ src/main/java/com/joinme/controller/ExampleController.java
@@ -0,0 +1,24 @@
+package com.joinme.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ExampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/{number}")
+    @ResponseBody
+    public String testPathVariable(@PathVariable("number") int number) {
+        return "Hello du bist nummer: " + number;
+    }
+}
