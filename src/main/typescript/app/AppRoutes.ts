871774801f595885691809e96578cf004d4666d7
Cleatuer <nicole.wresnik@gmail.com>
2016-11-27 20:28:50 +0100

    [Account Creation] create registration component

--- src/main/typescript/app/AppRoutes.ts
+++ src/main/typescript/app/AppRoutes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 
-import { DataResolver } from './app.resolver';
+import { DataResolver } from './AppResolver';
+import {RegistrationComponent} from "./registration/RegistrationComponent";
 
 
 export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
+  { path: 'register',  component: RegistrationComponent },
   { path: 'about', component: AboutComponent },
   {
     path: 'detail', loadChildren: () => System.import('./+detail').then((comp: any) => {
---
+++ src/main/typescript/app/registration/RegistrationData.ts
@@ -0,0 +1,7 @@
+
+export class RegistrationData {
+
+  public username: string;
+  public email: string;
+  public password: string;
+}
