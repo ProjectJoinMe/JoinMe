c72a616fad2298b8968e88d98acec30213233890
Cleatuer <nicole.wresnik@gmail.com>
2016-11-27 20:27:46 +0100

    [setup] rename to simpler naming convention + cleanup unused files

---
+++ src/main/typescript/app/home/HomeComponent.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { AppState } from '../AppService';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'home',  // <home></home>
+  providers: [
+  ],
+  styleUrls: [ './HomeComponent.css' ],
+  templateUrl: './HomeComponent.html'
+})
+export class HomeComponent {
+  constructor(private router: Router) {
+  }
+
+  goToRegistration() {
+    this.router.navigate(['/register']);
+  }
+}
