7b8afe7ac079d4e27757a008d5b52db9f72f995b
Cleatuer <nicole.wresnik@gmail.com>
2016-11-30 22:25:44 +0100

    [Account Creation] mark UserAccountRepository as repository

--- src/main/java/com/joinme/backend/accounts/repository/UserAccountRepository.java
+++ src/main/java/com/joinme/backend/accounts/repository/UserAccountRepository.java
@@ -3,7 +3,9 @@ package com.joinme.backend.accounts.repository;
 
 import com.joinme.backend.accounts.entity.UserAccount;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
 
     UserAccount findByUsername(String username);
