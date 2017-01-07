ad66ce4be61a63e263f8bc0d84b2fccdf3ac15b2
Cleatuer <nicole.wresnik@gmail.com>
2017-01-07 17:22:01 +0100

    [Account Creation] encrypt password on registration

--- src/main/java/com/joinme/backend/accounts/UserAccountCreationBean.java
+++ src/main/java/com/joinme/backend/accounts/UserAccountCreationBean.java
@@ -4,6 +4,7 @@ import com.joinme.backend.accounts.dto.AccountRegistrationData;
 import com.joinme.backend.accounts.entity.UserAccount;
 import com.joinme.backend.accounts.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +15,11 @@ public class UserAccountCreationBean implements UserAccountCreation {
     @Autowired
     private UserAccountRepository userAccountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     // TODO encrypt password using salt and so on -> see Spring security example
+
     @Override
     public void createUser(AccountRegistrationData accountRegistrationData) {
         UserAccount userAccount = convertRegistrationDataToAccount(accountRegistrationData);
@@ -25,7 +30,11 @@ public class UserAccountCreationBean implements UserAccountCreation {
         UserAccount userAccount = new UserAccount();
         userAccount.setEmail(accountRegistrationData.getEmail());
         userAccount.setUsername(accountRegistrationData.getUsername());
-        userAccount.setPassword(accountRegistrationData.getPassword());
+
+        String rawPassword = accountRegistrationData.getPassword();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        userAccount.setPassword(encodedPassword);
+
         return userAccount;
     }
 }
