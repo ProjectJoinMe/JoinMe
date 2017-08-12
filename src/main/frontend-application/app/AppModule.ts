19b8118e85f46e0280060cd989644eb481f8e94a
Cleatuer <nicole.wresnik@gmail.com>
2017-08-12 16:33:24 +0200

    [update ride] improved way of displaying the values of the update form

--- src/main/frontend-application/app/AppModule.ts
+++ src/main/frontend-application/app/AppModule.ts
@@ -4,6 +4,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
 import {removeNgStyles, createNewHosts, createInputTransfer} from '@angularclass/hmr';
+import {DatePipe} from '@angular/common';
 
 /*
  * Platform and Environment providers/directives/pipes
@@ -77,7 +78,8 @@ type StoreType = {
     providers: [ // expose our Services and Providers into Angular's dependency injection
         ENV_PROVIDERS,
         APP_PROVIDERS,
-        { provide: LOCALE_ID, useValue: "de-DE" }
+        { provide: LOCALE_ID, useValue: "de-DE" },
+        DatePipe
     ]
 })
 export class AppModule {
