73c12b8fc869f595be4cdae34d4d724a0b2280fd
Cleatuer <nicole.wresnik@gmail.com>
2017-12-29 17:21:25 +0100

    [notifications] send mails for notifications

--- src/main/java/com/joinme/frontend/api/controller/notifications/UserNotificationController.java
+++ src/main/java/com/joinme/frontend/api/controller/notifications/UserNotificationController.java
@@ -1,23 +1,15 @@
 package com.joinme.frontend.api.controller.notifications;
 
-import com.joinme.backend.accounts.UserProfileManager;
-import com.joinme.backend.accounts.dto.UserPasswordDto;
-import com.joinme.backend.accounts.dto.UserProfileDto;
 import com.joinme.backend.notifications.NotificationManagerBean;
 import com.joinme.backend.notifications.dto.UserNotificationDto;
 import com.joinme.frontend.api.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.MultipartConfigElement;
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
