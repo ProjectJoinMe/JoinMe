328f26f6429e830bccaac79d3cd0bac677a577df
Cleatuer <nicole.wresnik@gmail.com>
2018-01-07 10:36:16 +0100

    [proactive Matching] added LatLng attribute to PointOfInterest

--- src/main/java/com/joinme/backend/accounts/dto/PointOfInterestDto.java
+++ src/main/java/com/joinme/backend/accounts/dto/PointOfInterestDto.java
@@ -1,10 +1,16 @@
 package com.joinme.backend.accounts.dto;
 
+import com.joinme.backend.location.LatLng;
+
 import java.io.Serializable;
 
 public class PointOfInterestDto implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
     private String placeId;
     private String name;
+    private LatLng location;
 
     public String getName() {
         return name;
@@ -20,5 +26,13 @@ public class PointOfInterestDto implements Serializable{
 
     public void setPlaceId(String placeId) {
         this.placeId = placeId;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
     }
 }
---
+++ src/main/resources/sql/the_droppening.sql
@@ -0,0 +1,5 @@
+DROP TABLE user_notification;
+DROP TABLE rating;
+DROP TABLE ride_join;
+DROP TABLE ride;
+DROP TABLE user_account;
