9ae8ed7ae5b3862056c1b4c3ab37811f225d4d1f
Alexander Dietrich <diealb13@htl-kaindorf.ac.at>
2018-03-18 10:29:36 +0100

    fixes

--- src/main/frontend-application/app/+detail/detail.component.ts
+++ src/main/frontend-application/app/+detail/detail.component.ts
@@ -3,9 +3,9 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'detail',
     template: `
-    <h1>Hello from Detail</h1>
-    <router-outlet></router-outlet>
-  `
+        <h1>Hello from Detail</h1>
+        <router-outlet></router-outlet>
+    `
 })
 export class DetailComponent {
     constructor() {
